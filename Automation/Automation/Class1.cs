9e55ad97056a9c23234946fe7fd813c41fc795d3
Federico Palomero Garcia <fedepalo13@gmail.com>
2018-02-01 15:08:31 -0300

    first commit

---
+++ Automation/Automation/Class1.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation
+{
+    public class Class1
+    {
+        public void goToGoogle()
+        {
+            var driver = new ChromeDriver("C:/Users/federico.palomero/Documents/Projects/C#Automation/Automation/Automation");
+            driver.Navigate().GoToUrl("http://google.com");
+        }
+    }
+}
---
+++ Automation/Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Automation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var c = new Class1();
+            c.goToGoogle();
+        }
+    }
+}
